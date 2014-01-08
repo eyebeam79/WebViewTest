880cc0224c7f9597eb1d81327e451b05f8695edd
SDT1 <eyebeam79@naver.com>
2014-01-08 09:06:25 +0900

    4.4.5. 실습을 완료했습니다.

--- WebViewTest/ViewController.m
+++ WebViewTest/ViewController.m
@@ -8,11 +8,55 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate, UIWebViewDelegate>
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+
 
 @end
 
 @implementation ViewController
+
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
+{
+    if (navigationType == UIWebViewNavigationTypeLinkClicked)
+    {
+        NSLog(@"링크클릭은금지!");
+        return NO;
+    }
+    
+    return YES;
+}
+
+// 에러처리
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
+{
+    NSString *msg = [error localizedDescription];
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"에러" message:msg delegate:nil cancelButtonTitle:@"확인" otherButtonTitles: nil];
+    [alert show];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [self go:nil];
+    
+    return YES;
+}
+
+- (IBAction)go:(id)sender
+{
+    if ([self.textField.text length]>3)
+    {
+        NSURL *url = [NSURL URLWithString:self.textField.text];
+        NSURLRequest *request = [NSURLRequest requestWithURL:url];
+        [self.webView loadRequest:request];
+        
+        // 키보드감추기
+        [self.textField resignFirstResponder];
+    }
+
+}
+
 
 - (void)viewDidLoad
 {
